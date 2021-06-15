408ba4f3c573b758d82b908dbc82f4f4d0b3c934
Tyler Nguyen <tyler@magebinary.com>
2021-06-15 14:28:04 +0700

    SP-3704 Update snippet and modal URLs base on the LatitudePay+ configuration

--- templates/images_api/snippet.php
+++ templates/images_api/snippet.php
@@ -3,6 +3,6 @@
  * @var WC_LatitudeFinance_Method_Latitudepay $this
  */
 ?>
-<img class="lpay_snippet" src="<?php echo $this->getImagesApiUrl(); ?><?php echo $this->getSnippetPath(); ?>?amount=<?php echo $this->getAmount(); ?><?php if ($this->isFullBlock()): ?>&full_block=1<?php endif; ?>"
+<img class="lpay_snippet" src="<?php echo $this->getImagesApiUrl(); ?><?php echo $this->getSnippetPath(); ?>?amount=<?php echo $this->getAmount(); ?><?php if ($this->isFullBlock()): ?>&full_block=1<?php endif; ?><?php if($this->getPaymentTerm()): ?>?term=<?php echo $this->getPaymentTerm(); ?><?php endif; ?>"
      alt="<?php echo $this->getTitle(); ?>"/>
 
