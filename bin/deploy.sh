89a0916cbb4ab16fca0fb01f2647b926f0226a9b
Hiep Tran <tranquyhiep87@gmail.com>
2021-06-22 14:13:04 +0700

    Update CI

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-# export SSH_REMOTE_SERVER_USER=mage
-# export SSH_REMOTE_SERVER_IP=112.109.73.217
-# export SSH_REMOTE_SERVER_HOST=112.109.73.217
-# export SSH_REMOTE_SERVER_ROOT=/var/www/staging/wordpress-demo
-# export CIRCLE_PROJECT_REPONAME=woocommerce-latitudepay-genoapay
-
+export SSHPASS=$SSH_REMOTE_SERVER_PASSWORD
+export DEPLOY_BRANCH=$SSH_REMOTE_SERVER_GIT_BRANCH
 INVALID_PARAMETERS="\033[1;31mError:\033[0m Please make sure you've indicated correct parameters."
 
 # Path to plugin directory on the remote server
@@ -12,12 +8,13 @@ PLUGIN_DIR="${SSH_REMOTE_SERVER_ROOT}/wp-content/plugins/${CIRCLE_PROJECT_REPONA
 
 # Set Remote server SSH credentials (If this is a public Repo, you will want to set these as CircleCI environment variables)
 SSH_CREDS="${SSH_REMOTE_SERVER_USER}@${SSH_REMOTE_SERVER_HOST}"
+SSH_EXTRA="-o PubkeyAuthentication=Yes -o StrictHostKeyChecking=no"
 
 # Add the server IP to the known_hosts file
 # Required for CircleCI to allow SSH connections to remote server
 # example: ssh-keyscan 123.456.789.123 >> ~/.ssh/known_hosts
 ssh-keyscan ${SSH_REMOTE_SERVER_IP} >> ~/.ssh/known_hosts
-cd ~/latitude
+cd /var/www/html/wp-content/plugins/woocommerce-latitudepay-genoapay
 
 if [ $# -eq 0 ]
 	then
@@ -26,12 +23,13 @@ elif [ $1 == "--dry-run" ]
 	then
 		echo "Running dry-run deployment."
 		ssh -p22 ${SSH_CREDS} mkdir -p ${PLUGIN_DIR}
-		rsync --exclude={'.git/','bin/','.docker/','vendor/','log/','tests/*','.vscode/','.circleci/','.idea/'} --stats --dry-run --delete --progress -avz -e "ssh -p22" ./ ${SSH_CREDS}:${PLUGIN_DIR}
+		BUILD_COMMAND="cd ${PLUGIN_DIR} && git checkout ${DEPLOY_BRANCH} && git reset --hard origin/${DEPLOY_BRANCH} && git pull origin ${DEPLOY_BRANCH}"
+		sshpass -e ssh ${SSH_EXTRA} ${SSH_CREDS} -p${SSH_REMOTE_SERVER_PORT} $BUILD_COMMAND
 elif [ $1 == "live" ]
 	then
 		echo "Running actual deploy"
-		ssh -p22 ${SSH_CREDS} mkdir -p ${PLUGIN_DIR}
-		rsync --exclude={'.git/','bin/','.docker/','vendor/','log/','tests/','.vscode/','.circleci/','.idea/'} --stats --delete -avz -e "ssh -p22" ./ ${SSH_CREDS}:${PLUGIN_DIR}
+		BUILD_COMMAND="cd ${PLUGIN_DIR} && git checkout ${DEPLOY_BRANCH} && git reset --hard origin/${DEPLOY_BRANCH} && git pull origin ${DEPLOY_BRANCH}"
+		sshpass -e ssh ${SSH_EXTRA} ${SSH_CREDS} -p${SSH_REMOTE_SERVER_PORT} $BUILD_COMMAND
 else
 	echo -e ${INVALID_PARAMETERS};
 fi
