8dddd7a2034edae15e50628c59d93a38512e5586
Hiep Tran <tranquyhiep87@gmail.com>
2021-06-28 10:49:06 +0700

    Update

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 set -e
-echo "Installing the test environment..."
 
-docker-compose exec wordpress \
-	/bin/app/woocommerce/install.sh
-
-echo "Install composer packages"
+echo "Remove composer packages"
 docker-compose exec wordpress rm -rf composer.lock
 docker-compose exec wordpress rm -rf vendor/
 
 echo "Install composer packages"
 
 docker-compose exec wordpress composer install --ignore-platform-reqs
+
+echo "Installing the woocommerce..."
+docker-compose exec wordpress \
+	/bin/app/woocommerce/install.sh
