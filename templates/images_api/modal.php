00325ef4d98f072aa17f52a0ca370f0f9de07af8
Hiep Tran <tranquyhiep87@gmail.com>
2021-08-04 16:25:53 +0700

    Update code

--- templates/images_api/modal.php
+++ templates/images_api/modal.php
@@ -4,22 +4,12 @@
  */
 ?>
 <script>
-    !function () {
-        var e = document.querySelectorAll("img[src*='<?php echo $this->getImagesApiUrl(); ?>snippet.svg'], img[src*='<?php echo $this->getImagesApiUrl(); ?>api/banner'], img[src*='<?php echo $this->getImagesApiUrl(); ?>LatitudePayPlusSnippet.svg']");
-        [].forEach.call(
-            e, function (e) {
-                e.style.cursor = "pointer",
-                    e.addEventListener("click", handleClick)
-            })
-        function handleClick(e) {
-            if (0 == document.getElementsByClassName("lpay-modal-wrapper").length) {
-                var t = new XMLHttpRequest;
-                t.onreadystatechange = function () {
-                    4 == t.readyState && 200 == t.status && null != t.responseText && (document.body.insertAdjacentHTML("beforeend", t.responseText));
-                },
-                    t.open("GET", e.srcElement.getAttribute('src').replace('snippet.svg','modal.html'), !0),
-                    t.send(null)
-            } else document.querySelector(".lpay-modal-wrapper").style.display = "block"
-        }
-    }();
+    !function ($) {
+        $("img[src*='<?php echo $this->getImagesApiUrl(); ?>snippet.svg'], img[src*='<?php echo $this->getImagesApiUrl(); ?>api/banner'], img[src*='<?php echo $this->getImagesApiUrl(); ?>LatitudePayPlusSnippet.svg']").click(function(){
+            var url = $(this).attr('src').replace('snippet.svg','modal.html');
+            $.get(url,function(html){
+                $( "body" ).append(html);
+            });
+        });
+    }(jQuery);
 </script>
