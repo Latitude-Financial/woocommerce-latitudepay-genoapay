0ad38f5bb6d7fd3bcb4285358782a47a9d80104f
Hiep Tran <tranquyhiep87@gmail.com>
2021-08-05 19:23:30 +0700

    Update code

--- templates/images_api/modal.php
+++ templates/images_api/modal.php
@@ -3,13 +3,4 @@
  * @var WC_LatitudeFinance_Method_Latitudepay $this
  */
 ?>
-<script>
-    !function ($) {
-        $("img[src*='<?php echo $this->getImagesApiUrl(); ?>snippet.svg'], img[src*='<?php echo $this->getImagesApiUrl(); ?>api/banner'], img[src*='<?php echo $this->getImagesApiUrl(); ?>LatitudePayPlusSnippet.svg']").click(function(){
-            var url = $(this).attr('src').replace('snippet.svg','modal.html');
-            $.get(url,function(html){
-                $( "body" ).append(html);
-            });
-        });
-    }(jQuery);
-</script>
+<script src="<?php echo $this->getImagesApiUrl(); ?>util.js"></script>
